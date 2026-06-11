9ef1c96b47968e12ed379182a47a63bcb6e666d8
AbidRafiul <abidrafiul9@gmail.com>
2026-06-11 10:11:58 +0700

    add: fitur arsip loan return

--- express_arsip_be/routes/v1/arsip_dokumen/index.js
+++ express_arsip_be/routes/v1/arsip_dokumen/index.js
@@ -2,64 +2,68 @@ import express from "express";
 const router = express.Router();
 
 import documentCreate from "./document_create.js";
+import documentUpload from "./document_upload.js";
+import { uploadDocument } from "../../../middleware/upload_document.js";
+import documentUpdate from "./document_update.js";
+import documentDelete from "./document_delete.js";
+import documentGet from "./document_get.js";
+import documentDetail from "./document_detail.js";
+import documentVersionCreate from "./document_version_create.js";
+import documentVersionUpload from "./document_version_upload.js";
+import documentVersionGet from "./document_version_get.js";
+import loanCreate from "./archive_loan_create.js";
+import loanGet from "./archive_loan_get.js";
+import loanApprove from "./archive_loan_approve.js";
+import loanReturn from "./archive_loan_return.js";
 
 // POST /create - Create new document
 router.post("/create", documentCreate);
 
-import documentUpload from "./document_upload.js";
-import { uploadDocument } from "../../../middleware/upload_document.js";
 
 // POST /upload - Upload document file
 router.post("/upload", uploadDocument, documentUpload);
 
-import documentUpdate from "./document_update.js";
 
 // POST /update - Update document metadata
 router.post("/update", documentUpdate);
 
-import documentDelete from "./document_delete.js";
 
 // POST /delete - Soft delete document metadata
 router.post("/delete", documentDelete);
 
-import documentGet from "./document_get.js";
 
 // GET /get - Get document metadata
 router.get("/get", documentGet);
 
-import documentDetail from "./document_detail.js";
 
 // GET /detail - Get document detail with versions and loans
 router.get("/detail", documentDetail);
 
-import documentVersionCreate from "./document_version_create.js";
 
 // POST /version/create - Create new document version
 router.post("/version/create", documentVersionCreate);
 
-import documentVersionUpload from "./document_version_upload.js";
 
 // POST /version/upload - Upload document file and create version
 router.post("/version/upload", uploadDocument, documentVersionUpload);
 
-import documentVersionGet from "./document_version_get.js";
 
 // GET /version/get - Get document versions
 router.get("/version/get", documentVersionGet);
 
-import loanCreate from "./archive_loan_create.js";
 
 // POST /loan/create - Create new archive loan
 router.post("/loan/create", loanCreate);
 
-import loanGet from "./archive_loan_get.js";
 
 // GET /loan/get - Get archive loans
 router.get("/loan/get", loanGet);
 
-import loanApprove from "./archive_loan_approve.js";
 
 // POST /loan/approve - Approve or reject archive loan
 router.post("/loan/approve", loanApprove);
+
+// POST /loan/return - Mark archive loan as returned
+router.post("/loan/return", loanReturn);
 
 export default router;
